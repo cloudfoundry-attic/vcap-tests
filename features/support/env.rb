7c28f42a25add3bc583934b309f9c8a9f7e1f13b
Michael <github@jexp.de>
2011-08-03 10:53:25 -0700

    Neo4j tests
    Squashed commit of the following:
    
    commit 4b285fea728c62830c49bd0b2eae66ae9a17aaed
    Merge: 4c1e548 fb6860e
    Author: Patrick Bozeman <pbozeman@vmware.com>
    Date:   Tue Aug 2 11:49:50 2011 -0700
    
        Merge remote-tracking branch 'jexp/master'
    
        Contains manual edits during the merge to bring up-to-date
        with the rest of vcap-tests and vcap-services.
    
        Conflicts:
        	features/support/env.rb
        	features/support/testconfig.yml
    
        Change-Id: I1c22050996f37f82825bf382ec75c1ad4325d70c
    
    commit fb6860e935640e20bb46a6fdf90fa5e0326f51ef
    Author: Michael Hunger <github@jexp.de>
    Date:   Fri May 27 23:02:19 2011 +0200
    
        finished simple neo4j cucumber test
    
    commit 87cfa4f6f1807ce698d01412ce3d1d17bf712062
    Author: CloudFoundry <cloud@cloudfoundry.hosted.neo4j.org>
    Date:   Fri May 27 14:43:40 2011 +0200
    
        updated neo4j test app
    
    commit dcaf13c88450e6068386ad6a8171e9879a5c7915
    Author: Michael Hunger <github@jexp.de>
    Date:   Thu May 26 04:22:03 2011 +0200
    
        added cucumber test for simple neo4j application
    
    commit e90289a9596e6c6e9b7179b51463d3736749bd7a
    Author: Michael Hunger <github@jexp.de>
    Date:   Fri May 13 15:23:55 2011 +0200
    
        added neo4j sample app
    
    Change-Id: I8e438f1b2c3284006c1ea41ab81fe138dbdfd44d

--- features/support/env.rb
+++ features/support/env.rb
@@ -45,6 +45,7 @@ SIMPLE_ERLANG_APP = "mochiweb_test"
 SIMPLE_LIFT_APP = "simple-lift-app"
 LIFT_DB_APP = "lift-db-app"
 TOMCAT_VERSION_CHECK_APP="tomcat-version-check-app"
+NEO4J_APP = "neo4j_app"
 SIMPLE_PYTHON_APP = "simple_wsgi_app"
 PYTHON_APP_WITH_DEPENDENCIES = "wsgi_app_with_requirements"
 SIMPLE_DJANGO_APP = "simple_django_app"
@@ -119,6 +120,10 @@ end
 
 After("@creates_tomcat_version_check_app") do
   AppCloudHelper.instance.delete_app_internal TOMCAT_VERSION_CHECK_APP
+end
+
+After("@creates_neo4j_app") do
+  AppCloudHelper.instance.delete_app_internal NEO4J_APP
 end
 
 After("@creates_wsgi_app") do
@@ -207,6 +212,7 @@ class AppCloudHelper
     delete_app_internal(SIMPLE_LIFT_APP)
     delete_app_internal(LIFT_DB_APP)
     delete_app_internal(TOMCAT_VERSION_CHECK_APP)
+    delete_app_internal(NEO4J_APP)
     delete_app_internal(SIMPLE_PYTHON_APP)
     delete_app_internal(PYTHON_APP_WITH_DEPENDENCIES)
     delete_app_internal(SIMPLE_DJANGO_APP)
@@ -622,6 +628,17 @@ class AppCloudHelper
        service_manifest
      end
 
+  def provision_neo4j_service token
+    name = "#{@namespace}#{@app || 'simple_neo4j_app'}neo4j"
+    @client.create_service(:neo4j, name)
+    service_manifest = {
+     :vendor=>"neo4j",
+     :tier=>"free",
+     :version=>"1.4",
+     :name=>name
+    }
+  end
+
   def provision_postgresql_service
     name = "#{@namespace}#{@app || 'simple_db_app'}postgresql"
     @client.create_service(:postgresql, name)
@@ -779,10 +796,15 @@ class AppCloudHelper
     easy
   end
 
-  def post_record uri, data_hash
+  def post_record_no_close uri, data_hash
     easy = Curl::Easy.new
     easy.url = uri
     easy.http_post(data_hash.to_json)
+    easy
+  end
+
+  def post_record uri, data_hash
+    easy = post_record_no_close(uri,data_hash)
     easy.close
   end
 
