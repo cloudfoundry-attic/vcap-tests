f6c943ff6b200572ba35fa72d7d0bde882af15d9
Patrick Bozeman <pbozeman@vmware.com>
2011-08-16 16:34:11 -0700

    whitespace fixes from the python merge
    Change-Id: I19046207338356480005eab8b543b82b8583c054

--- apps/simple_django_app/urls.py
+++ apps/simple_django_app/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-		url(r'^$', direct_to_template, {'template': 'index.html'}),
+                url(r'^$', direct_to_template, {'template': 'index.html'}),
 )
