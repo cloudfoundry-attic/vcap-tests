db26701ad8b1c48eb1e698950beb4585c8992b81
Winston Koh <wkoh@vmware.com>
2011-10-28 09:17:15 -0700

    retired the old /apps directory
    Change-Id: Ie2dc6e9eb4ecc2a8ac06d02ba20a1f6f5057d659

--- apps/simple_db_app/lib/models/user.rb
+++
@@ -1,9 +0,0 @@
-require 'datamapper'
-
-class User
-  include DataMapper::Resource
-
-  property :id,           String, :key => true, :required => true, :unique => true
-  property :desc,         String
-  
-end
--- apps/wsgi_app_with_requirements/wsgi.py
+++
@@ -1,9 +0,0 @@
-import bottle
-
-def application(environ, start_response):
-    data = "Hello from VCAP"
-    start_response("200 OK", [
-        ("Content-Type", "text/plain"),
-        ("Content-Length", str(len(data)))
-    ])
-    return iter([data])
