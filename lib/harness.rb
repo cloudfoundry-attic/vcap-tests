9eaaef1cf4a6216fa73c0e4dc2bc441d18982f25
Pin Xie <pxie@vmware.com>
2012-04-05 20:51:21 +0800

    init rspec based bvt solution
    1. add one case to add-user/users/delete-user for admin privilege
    2. design harness library for test case common usage
    3. store all *_spec.rb in spec/ directory
    4. simplify Rakefile
    5. add environment checker before running rake tests
    
    Change-Id: I8f16eafe9a79436dad95c0a36af021f9036ff181
    Signed-off-by: Pin Xie <pxie@vmware.com>

---
+++ lib/harness.rb
@@ -0,0 +1,19 @@
+
+module BVT
+  module Harness
+    VCAP_BVT_HOME = File.join(ENV['HOME'], '.bvt')
+    VCAP_BVT_CONFIG_FILE = File.join(VCAP_BVT_HOME, "config.yml")
+    VCAP_BVT_PROFILE_FILE = File.join(VCAP_BVT_HOME, "profile.yml")
+    VCAP_BVT_LOG_FILE = File.join(VCAP_BVT_HOME, "bvt.log")
+    LOGGER_LEVEL = :debug
+  end
+end
+
+require "harness/user_helper"
+require "harness/color_helper"
+require "harness/rake_helper"
+
+require "harness/client"
+require "harness/app"
+require "harness/service"
+require "harness/user"
