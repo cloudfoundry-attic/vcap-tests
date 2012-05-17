57e345043b526d0980fbe0f8f64288be6a11d49d
Yang Yang <yangy@vmware.com>
2012-05-17 01:07:25 -0700

    modified vblob bvt to use app_sinatra_service and bump assets pointer
    Change-Id: I645753d83343f4f037c5b909b6ac74c71669edff

--- features/step_definitions/vblob_steps.rb
+++ features/step_definitions/vblob_steps.rb
@@ -15,21 +15,21 @@ Given /^I have deployed a vblob application that is bound to this service$/ do
 end
 
 Given /^I create a container in my vblob service through my application$/ do
-  uri = get_uri(@app, 'container1')
+  uri = get_uri(@app, 'service/vblob/container1')
   r = post_uri(uri,'dummy') # here post_uri requires payload, but our service safely ignores the payload, thus really some dummy data to make post_uri happy
   r.response_code.should == 200
   r.close
 end
 
 When /^I create an file in my vblob service through my application$/ do
-  uri = get_uri(@app, 'container1/file1')
+  uri = get_uri(@app, 'service/vblob/container1/file1')
   r = post_uri(uri, 'abc')
   r.response_code.should == 200
   r.close
 end
 
 Then /^I should be able to get the file$/ do
-  uri = get_uri @app, "container1/file1"
+  uri = get_uri @app, "service/vblob/container1/file1"
   r = get_uri_contents uri
   r.should_not == nil
   r.response_code.should == 200
